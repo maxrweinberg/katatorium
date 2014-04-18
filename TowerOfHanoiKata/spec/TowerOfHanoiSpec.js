9f09d8c60e0b986db8a599c89402855c88c2c7fa
Max <maxxthepenguin@gmail.com>
2014-04-17 23:39:12 -0400

    Iterative Done
    Iterative solution done, but not more performant. Seemingly less
    performant. Weird

--- TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
+++ TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
@@ -7,4 +7,13 @@ describe("HANOI!!!", function() {
 		expect(hanoi(1,3,2,2)).toBe("12,13,23,");
 		expect(hanoi(3,1,2,2)).toBe("32,31,21,");
 	});
+	
+	it("It gives moves to solve a one ring tower of hanoi iteratively", function() {
+		expect(hanoi(1,3,2,1)).toBe("13,");
+	});
+	
+	it("It gives moves to solve two ring towers of hanoi iteratively", function() {
+		expect(iterativeHanoi(1,3,2,2)).toBe("12,13,23,");
+		expect(iterativeHanoi(3,1,2,2)).toBe("32,31,21,");
+	});
 });
--- TowerOfHanoiKata/src/TowerOfHanoi.js
+++ TowerOfHanoiKata/src/TowerOfHanoi.js
@@ -1,7 +1,27 @@
 function hanoi(from, to, using, rings){
-	if(rings > 0){
-		return hanoi(from, using, to, rings - 1) + from + to + "," + hanoi(using, to, from, rings - 1)
+	if(rings === 0){
+		return "";
 	} else {
-		return ""
+		return hanoi(from, using, to, rings - 1) + from + to + "," + hanoi(using, to, from, rings - 1);
 	}
+}
+
+function iterativeHanoi(from, to, using, rings){
+	var stack = [
+		[from, to, using, rings, false]
+	];
+	var str = "";
+	
+	while(stack.length > 0){
+		var current = stack.pop();
+		if(current[4] === true){
+			str += current[0] + '' + current[1] + ',';
+		} else if(current[3] > 0){
+			stack.push([current[2],current[1],current[0],current[3]-1,false]);
+			stack.push([current[0],current[1],current[2],current[3],true]);
+			stack.push([current[0],current[2],current[1],current[3]-1,false]);
+		}
+	}
+	
+	return str;
 }
