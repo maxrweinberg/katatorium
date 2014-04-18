855fa03bbe8868914c4ad64ae052143620f02c5e
Max <maxxthepenguin@gmail.com>
2014-04-17 22:24:51 -0400

    Test improvements

--- TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
+++ TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
@@ -1,6 +1,9 @@
 describe("HANOI!!!", function() {
-    it("It gives moves to solve the tower of hanoi", function() {
+    it("It gives moves to solve a one ring tower of hanoi", function() {
 		expect(hanoi(1,3,2,1)).toBe("1 to 3, ");
+	});
+	
+	it("It gives moves to solve two ring towers of hanoi", function() {
 		expect(hanoi(1,3,2,2)).toBe("1 to 2, 1 to 3, 2 to 3, ");
 		expect(hanoi(3,1,2,2)).toBe("3 to 2, 3 to 1, 2 to 1, ");
 	});
