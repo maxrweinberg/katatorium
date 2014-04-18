0c122cfce01da1be8c7bd10875b07168159cb48e
Max <maxxthepenguin@gmail.com>
2014-04-17 22:31:28 -0400

    Can do more rings
    Went from 23 to 25 rings able to be done in chrome

--- TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
+++ TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
@@ -1,10 +1,10 @@
 describe("HANOI!!!", function() {
     it("It gives moves to solve a one ring tower of hanoi", function() {
-		expect(hanoi(1,3,2,1)).toBe("1 to 3, ");
+		expect(hanoi(1,3,2,1)).toBe("13,");
 	});
 	
 	it("It gives moves to solve two ring towers of hanoi", function() {
-		expect(hanoi(1,3,2,2)).toBe("1 to 2, 1 to 3, 2 to 3, ");
-		expect(hanoi(3,1,2,2)).toBe("3 to 2, 3 to 1, 2 to 1, ");
+		expect(hanoi(1,3,2,2)).toBe("12,13,23,");
+		expect(hanoi(3,1,2,2)).toBe("32,31,21,");
 	});
 });
--- TowerOfHanoiKata/src/TowerOfHanoi.js
+++ TowerOfHanoiKata/src/TowerOfHanoi.js
@@ -1,6 +1,6 @@
 function hanoi(from, to, using, rings){
 	if(rings > 0){
-		return hanoi(from, using, to, rings - 1) + from + " to " + to + ", " + hanoi(using, to, from, rings - 1)
+		return hanoi(from, using, to, rings - 1) + from + to + "," + hanoi(using, to, from, rings - 1)
 	} else {
 		return ""
 	}
