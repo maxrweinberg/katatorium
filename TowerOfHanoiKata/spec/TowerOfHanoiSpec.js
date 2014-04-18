25801b668b3511483fcd992a9eaf344677e0a2e0
Max <maxxthepenguin@gmail.com>
2014-04-18 08:19:13 -0400

    3 ring test

--- TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
+++ TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
@@ -1,19 +1,27 @@
 describe("HANOI!!!", function() {
-    it("It gives moves to solve a one ring tower of hanoi", function() {
+    it("gives moves to solve a one ring tower of hanoi", function() {
 		expect(hanoi(1,3,2,1)).toBe("13,");
 	});
 	
-	it("It gives moves to solve two ring towers of hanoi", function() {
+	it("gives moves to solve two ring towers of hanoi", function() {
 		expect(hanoi(1,3,2,2)).toBe("12,13,23,");
 		expect(hanoi(3,1,2,2)).toBe("32,31,21,");
 	});
 	
-	it("It gives moves to solve a one ring tower of hanoi iteratively", function() {
+	it("gives moves to solve a three ring tower of hanoi", function() {
+		expect(hanoi(1,2,3,3)).toBe("12,13,23,12,31,32,12,");
+	});
+	
+	it("gives moves to solve a one ring tower of hanoi iteratively", function() {
 		expect(hanoi(1,3,2,1)).toBe("13,");
 	});
 	
-	it("It gives moves to solve two ring towers of hanoi iteratively", function() {
+	it("gives moves to solve two ring towers of hanoi iteratively", function() {
 		expect(iterativeHanoi(1,3,2,2)).toBe("12,13,23,");
 		expect(iterativeHanoi(3,1,2,2)).toBe("32,31,21,");
+	});
+	
+	it("gives moves to solve a three ring tower of hanoi iteratively", function() {
+		expect(iterativeHanoi(1,2,3,3)).toBe("12,13,23,12,31,32,12,");
 	});
 });
