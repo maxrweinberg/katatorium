2e52c7c3d0e40f6033db1c697a0f8d9e7bc7bca1
Max <maxxthepenguin@gmail.com>
2014-04-17 21:57:40 -0400

    Adding hanoi

---
+++ TowerOfHanoiKata/spec/TowerOfHanoiSpec.js
@@ -0,0 +1,5 @@
+describe("HANOI!!!", function() {
+    it("It gives moves to solve the tower of hanoi", function() {
+		expect(hanoi(1,3,2,1)).toBe("1 to 3,");
+	});
+});
---
+++ TowerOfHanoiKata/src/TowerOfHanoi.js
@@ -0,0 +1,3 @@
+function hanoi(from, to, using, rings){
+	return "Implementing in a second"
+}
